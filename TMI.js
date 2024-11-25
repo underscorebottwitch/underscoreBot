23afa2922023c374ab02d489c43eee5f10071bf8
underscorebottwitch <underscorebottwitch@gmail.com>
2024-11-25 18:47:58 +1100

    Delete TMI.js

--- TMI.js
+++
@@ -1,8 +0,0 @@
-const client = new tmi.Client({
-  options: { debug: true },
-  identity: {
-      username: underscoreunderscorebot,
-      password: oauth:mq9ffvxqr48mlmt5a3njpum03oz723
-  },
-  channels: [FacetiousAnarchist]
-});
