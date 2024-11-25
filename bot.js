c9c849a9b61bbf03a42d42cc181cdf7450035323
underscorebottwitch <underscorebottwitch@gmail.com>
2024-11-25 18:29:21 +1100

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,6 +5,6 @@ const axios = require('axios');
 
 // Environment variables
 const TWITCH_BOT_USERNAME = underscoreunderscorebot;
-const TWITCH_OAUTH_TOKEN = poauth:mq9ffvxqr48mlmt5a3njpum03oz723;
+const TWITCH_OAUTH_TOKEN = oauth:mq9ffvxqr48mlmt5a3njpum03oz723;
 const TWITCH_CHANNEL_NAME = FacetiousAnarchist;
 const ABACUS_API_KEY = s2_846ced8801d646f38b086ceea690b71a
