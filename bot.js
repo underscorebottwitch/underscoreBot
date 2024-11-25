dbf0c178b5a1c6496d8a0a5600a0bbfdc209cb8d
underscorebottwitch <underscorebottwitch@gmail.com>
2024-11-25 18:10:32 +1100

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ const tmi = require('tmi.js');
 const axios = require('axios');
 
 // Environment variables
-const TWITCH_BOT_USERNAME = process.env.TWITCH_BOT_USERNAME;
-const TWITCH_OAUTH_TOKEN = process.env.TWITCH_OAUTH_TOKEN;
-const TWITCH_CHANNEL_NAME = process.env.TWITCH_CHANNEL_NAME;
-const ABACUS_API_KEY = process.env.ABACUS_API_KEY;
+const TWITCH_BOT_USERNAME = process.env.underscoreunderscorebot;
+const TWITCH_OAUTH_TOKEN = process.env.oauth:mq9ffvxqr48mlmt5a3njpum03oz723;
+const TWITCH_CHANNEL_NAME = process.env.FacetiousAnarchist;
+const ABACUS_API_KEY = process.env.s2_846ced8801d646f38b086ceea690b71a;
